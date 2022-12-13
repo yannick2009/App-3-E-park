055dae08717b6a9ef8ab28db6d00b8f55cbea126
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2022-12-13 15:16:58 +0000

    error handler file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const path = require('node:path');
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -9,8 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '30kb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('*', (next) => {
-  next();
+app.all('*', (req, res, next) => {
+  next(
+    new AppError(
+      `${req.originalUrl} n'existe pas sur notre SERVER`,
+      404
+    )
+  );
 });
 
 module.exports = app;
