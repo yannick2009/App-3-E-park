6b7273a5d75f261508228939c20e15a1425b170b
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2022-12-31 05:18:00 +0000

    button animation

--- client/react-js/src/components/Button.js
+++ client/react-js/src/components/Button.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Button.scss";
 
 function Button() {
+  // USESTATES
+  const [status, setStatus] = useState("Libre");
+
+  const parkCar = () => {
+    setStatus("Occupé");
+  };
+  const unparkCar = () => {
+    setStatus("Libre");
+  };
+
   return (
     <div className="buttons">
-      <div className="status">
-        <p>Libre</p>
+      <div className={status === "Libre" ? "status-true" : "status-false"}>
+        <p>{status}</p>
       </div>
       <div className="btn">
         {/* TO PARK */}
-        <button className="park">Garer</button>
+        <button className="park" onClick={parkCar}>
+          Garer
+        </button>
         {/* TO UNPARK */}
-        <button className="unpark">Partir</button>
+        <button className="unpark" onClick={unparkCar}>
+          Partir
+        </button>
       </div>
     </div>
   );
